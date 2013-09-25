329a49cd3e0ff954014c8cb07e60b1c991569a39
mg.kedzie <mg.kedzie@gmail.com>
2013-09-25 14:45:58 -0500

    print book title

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello World ");
 		System.out.append('.');
+		Book b = new Book("GIT");
+		System.out.println(b.getTitle());
 	}
 
 }
