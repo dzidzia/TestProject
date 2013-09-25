fe451ff366a65f0fa29e6b9a7cdd9ee6d4901176
mg.kedzie <mg.kedzie@gmail.com>
2013-09-25 15:09:44 -0500

    made change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello World ");
 		System.out.append('.');
-		Book b = new Book("GIT");
+		Book b = new Book("GIT2");
 		System.out.println(b.getTitle());
 	}
 
