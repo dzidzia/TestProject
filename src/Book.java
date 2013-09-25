0fae3d0b457590d5479851b12eeca5049ba786eb
mg.kedzie <mg.kedzie@gmail.com>
2013-09-25 15:33:18 -0500

    added publisher

--- src/Book.java
+++ src/Book.java
@@ -3,6 +3,7 @@ public class Book {
 
 	private String title;
 	private int pages;
+	private static String publisher="Home Inc.";
 	public Book(String s) {title = s;}
 	public String getTitle(){return title;}
 
