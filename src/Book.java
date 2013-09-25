3f1de0e9f0e3f7fa68b7c0785d7d6b11d7f7c09d
mg.kedzie <mg.kedzie@gmail.com>
2013-09-25 15:43:21 -0500

    date

--- src/Book.java
+++ src/Book.java
@@ -4,7 +4,7 @@ public class Book {
 	
 	private String title;
 	private int pages;
-	private static String publisher="Home Inc.";
+	private static String publisher="Home Inc.2013";
 	public Book(String s) {title = s;}
 	public String getTitle(){return title;}
 
