0c4d7925438ba20e24c2146bde261e841f05b1ba
mg.kedzie <mg.kedzie@gmail.com>
2013-09-26 16:20:40 -0500

    constructor init pages

--- src/Book.java
+++ src/Book.java
@@ -4,8 +4,9 @@ public class Book {
 	
 	private String title;
 	private int pages;
-	private static String publisher="Home Inc.2013";
-	public Book(String s) {title = s;}
-	public String getTitle(){return title;}
 
+	private static String publisher="Home Inc.";
+	public Book(String s) {title = s; pages = 100; }
+
+	public String getTitle(){return title;}
 }
